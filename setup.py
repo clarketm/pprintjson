53cbf0755d19dfce5a7093219c055d2b665556af
Travis Clarke <travis.m.clarke@gmail.com>
2018-09-07 14:59:47 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pprintjson",
+    version="0.0.1",
+    author="Travis Clarke",
+    author_email="travis.m.clarke@gmail.com",
+    description="A json pretty printer for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clarketm/pprintjson",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

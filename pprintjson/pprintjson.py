8cc59a340303d92237cbdb2dc47974e6b6f94402
Travis Clarke <travis.m.clarke@gmail.com>
2018-11-05 13:43:54 -0800

    change default type of file parameter to None and use stdout if None

--- pprintjson/pprintjson.py
+++ pprintjson/pprintjson.py
@@ -1,20 +1,22 @@
 """pprintjson module"""
 
-from sys import stdout, argv
 from json import dumps, loads
-from pygments import highlight
-from pygments.lexers import JsonLexer
-from pygments.formatters import TerminalFormatter
+from sys import stdout, argv
 from typing import Dict, IO, List, Union
+
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import JsonLexer
 
 
 def pprintjson(
     obj: Union[Dict, List],
     indent: int = 4,
     end: str = "\n",
-    file: IO = stdout,
+    file: IO = None,
     flush: bool = False,
 ) -> None:
+    file = stdout if file is None else file
     json = dumps(obj, indent=indent)
     if file.isatty():
         json = highlight(json, JsonLexer(), TerminalFormatter())

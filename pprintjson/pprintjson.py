0ff0a491b799f7a799576f42cbfbaecc4252b110
Travis Clarke <travis.m.clarke@gmail.com>
2018-11-08 12:08:58 -0800

    add support for variable obj arguments

--- pprintjson/pprintjson.py
+++ pprintjson/pprintjson.py
@@ -10,17 +10,17 @@ from pygments.lexers import JsonLexer
 
 
 def pprintjson(
-    obj: Union[Dict, List],
+    *obj: Union[Dict, List],
     indent: int = 4,
     end: str = "\n",
     file: IO = None,
     flush: bool = False,
 ) -> None:
     file = stdout if file is None else file
-    json = dumps(obj, indent=indent)
+    json = [dumps(o, indent=indent) for o in obj]
     if file.isatty():
-        json = highlight(json, JsonLexer(), TerminalFormatter())
-    print(json, end=end, file=file, flush=flush)
+        json = [highlight(j, JsonLexer(), TerminalFormatter()) for j in json]
+    print(*json, end=end, file=file, flush=flush)
 
 
 if __name__ == "__main__":
